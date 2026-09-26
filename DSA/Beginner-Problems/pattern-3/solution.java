49768fb8d11fed45c54682c3fe06c501c05db3cb
Dilip Kumar <157205347+its-Dilip-Kumar@users.noreply.github.com>
2026-09-26 14:17:07 +0530

    Sync 975. Pattern 3 [DSA] (TUFHub)

---
+++ DSA/Beginner-Problems/pattern-3/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void pattern3(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j+1);
+            }
+            System.out.println();
+        }
+    }
+}
